f5a5e8fcae4912cf9d61e91c22d6a6076a213a37
Amolika28-Kale <amolikakale234@gmail.com>
2026-01-10 23:00:37 +0530

    employee dashboard backend changes

--- ita-erp-backend/routes/dashboardRoutes.js
+++ ita-erp-backend/routes/dashboardRoutes.js
@@ -3,8 +3,12 @@ const auth = require("../middleware/authMiddleware");
 const role = require("../middleware/roleMiddleware");
 const ctrl = require("../controllers/dashboardController");
 
-router.get("/", auth, role("admin"), ctrl.getAdminStats);
+router.use(auth); // ✅ APPLY AUTH TO ALL
 
+// ⚠️ SPECIFIC ROUTE FIRST
 router.get("/employee", role("employee"), ctrl.employeeDashboard);
+
+// GENERAL ADMIN ROUTE AFTER
+router.get("/", role("admin"), ctrl.getAdminStats);
 
 module.exports = router;
