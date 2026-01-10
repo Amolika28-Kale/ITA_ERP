7ae41f8a9f7d5d7b8704220e45fae0859513771b
Amolika28-Kale <amolikakale234@gmail.com>
2026-01-10 18:42:37 +0530

    backend changes

--- ita-erp-backend/models/User.js
+++ ita-erp-backend/models/User.js
@@ -1,19 +1,38 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  role: {
-    type: String,
-    enum: ["admin", "manager", "employee"],
-    default: "employee"
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "manager", "employee"],
+      default: "employee"
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true
+    },
+
+    teamId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Team",
+      default: null
+    }
   },
-  isActive: { type: Boolean, default: true },
-  teamId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Team"
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
