794da5679ca67b870a69968a5956f7c69b61fef6
Amolika28-Kale <amolikakale234@gmail.com>
2026-01-10 22:52:58 +0530

    employee dashboard backend

--- ita-erp-backend/controllers/dashboardController.js
+++ ita-erp-backend/controllers/dashboardController.js
@@ -22,3 +22,24 @@ exports.getAdminStats = async (req, res) => {
     activeProjects
   });
 };
+
+
+exports.employeeDashboard = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const total = await Task.countDocuments({ assignedTo: userId });
+    const todo = await Task.countDocuments({ assignedTo: userId, status: "todo" });
+    const inProgress = await Task.countDocuments({ assignedTo: userId, status: "in-progress" });
+    const completed = await Task.countDocuments({ assignedTo: userId, status: "completed" });
+
+    res.json({
+      totalTasks: total,
+      todo,
+      inProgress,
+      completed
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Employee dashboard error" });
+  }
+};
--- ita-erp-backend/routes/dashboardRoutes.js
+++ ita-erp-backend/routes/dashboardRoutes.js
@@ -5,4 +5,6 @@ const ctrl = require("../controllers/dashboardController");
 
 router.get("/", auth, role("admin"), ctrl.getAdminStats);
 
+router.get("/employee", role("employee"), ctrl.employeeDashboard);
+
 module.exports = router;
