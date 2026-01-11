9c1c55d7d35d70051a4f59d9fecd4484da67ec49
Amolika28-Kale <amolikakale234@gmail.com>
2026-01-12 01:28:48 +0530

    my projects api

--- ita-erp-backend/routes/projectRoutes.js
+++ ita-erp-backend/routes/projectRoutes.js
@@ -7,6 +7,8 @@ router.use(auth);
 
 router.post("/", role("admin", "manager"), ctrl.createProject);
 router.get("/", ctrl.getProjects);
+router.get("/my-projects", auth, ctrl.getMyProjects);
+
 router.put("/:id", role("admin", "manager"), ctrl.updateProject);
 router.patch("/:id/archive", role("admin"), ctrl.archiveProject);
 router.patch("/:id/status", role("admin", "manager"), ctrl.updateProjectStatus);
