5501ad4e0ed223e113413105c437a1fa3544d721
Amolika28-Kale <amolikakale234@gmail.com>
2026-01-14 00:03:13 +0530

    routes changes

--- ita-erp-backend/server.js
+++ ita-erp-backend/server.js
@@ -27,6 +27,6 @@ app.use("/api/settings", require("./routes/settingRoutes"));
 app.use("/api/notifications", require("./routes/notificationRoutes"));
 app.use("/api/attendance", require("./routes/attendenceRoutes"));
 app.use("/api/admin", require("./routes/adminAttendanceRoutes"));
-app.use("/api", require("./routes/taskReminderRoutes"));
+// app.use("/api", require("./routes/taskReminderRoutes"));
 
 app.listen(5000, () => console.log("Server running"));
